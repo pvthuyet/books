d457a1ad17ddf387c6cfb2909d32452b35462a2a
pvthuyet <pvthuyet@gmail.com>
2021-04-14 16:19:03 +0700

    update

--- boost.asio/chapter01/example01/client.cpp
+++ boost.asio/chapter01/example01/client.cpp
@@ -5,6 +5,26 @@ using namespace boost;
 
 int main()
 {
-	std::cout << "Boost.asio hello from client\n";
+	// Step 1. Assume that the client application has already
+	// obtained the IP-address and the protocal port number
+	std::string raw_ip_address = "172.0.0.1";
+	unsigned short port_number = 3333;
+
+	boost::system::error_code ec;
+
+	// Step2:
+	asio::ip::address ip_address = asio::ip::address::from_string(raw_ip_address, ec);
+	if (ec.value() != 0) {
+		std::cout << "Failed to parse the IP address. Error code = "
+			<< ec.value() << ". Message: "
+			<< ec.message();
+		return ec.value();
+	}
+
+	// step3
+	asio::ip::tcp::endpoint ep(ip_address, port_number);
+
+	// step4
+
 	return EXIT_SUCCESS;
 }
--- boost.asio/chapter01/example01/server.cpp
+++ boost.asio/chapter01/example01/server.cpp
@@ -5,6 +5,15 @@ using namespace boost;
 
 int main()
 {
-	std::cout << "Boost.asio hello from server\n";
+	// step 1
+	unsigned short port_number = 3333;
+	
+	// step 2
+	asio::ip::address ip_address = asio::ip::address_v6::any();
+
+	// step 3
+	asio::ip::tcp::endpoint ep(ip_address, port_number);
+
+	// step 4
 	return EXIT_SUCCESS;
 }
