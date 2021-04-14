f1a55eedfa5d63c47d4ce7a2f5ce50c10f257e76
pvthuyet <pvthuyet@gmail.com>
2021-04-14 15:04:50 +0700

    boost.asio: example01

---
+++ boost.asio/chapter01/example01/build.bat
@@ -0,0 +1,4 @@
+cmake -Bbuild
+cmake --build ./build
+"./build/Debug/server.exe"
+"./build/Debug/client.exe"
---
+++ boost.asio/chapter01/example01/server.cpp
@@ -0,0 +1,10 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+using namespace boost;
+
+int main()
+{
+	std::cout << "Boost.asio hello from server\n";
+	return EXIT_SUCCESS;
+}
