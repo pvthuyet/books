5ae72ae3f7d5725d27ab1db4032fc95d53abfeae
pvthuyet <pvthuyet@gmail.com>
2021-07-06 11:35:15 +0700

    brokerless client

---
+++ zeromq/4. Reliable Request-Reply Patterns/4.9 Brokerless Reliability (Freelance Pattern)/flclient1.cpp
@@ -0,0 +1,94 @@
+#include <Windows.h>
+#include <fmt/core.h>
+#include <vld.h>
+#include <zhelpers.hpp>
+#include <zmsg.hpp>
+#include <memory>
+#include <thread>
+
+#define REQUEST_TIMEOUT     1000
+#define MAX_RETRIES         3       //  Before we abandon
+
+class client
+{
+private:
+	zmq::context_t ctx_;
+	std::vector<std::string> servers_;
+	std::unique_ptr<zmq::socket_t> sock_;
+public:
+	client() :
+		ctx_(1)
+	{
+	}
+
+	void add(std::string_view addr)
+	{
+		servers_.emplace_back(addr);
+	}
+
+	void send_request()
+	{
+		zmsg msg("Hello world");
+		if (servers_.size() == 1) { // try 3 times if 1 server
+			for (int i = 0; i < MAX_RETRIES; ++i) {
+				auto reply = try_request(servers_[0], msg);
+				if (reply) {
+					reply->dump();
+					return;
+				}
+				fmt::print("W: no response from {}. Retring...\n", servers_[0]);
+			}
+		}
+		else { // try all servers
+			for (auto& ep : servers_) {
+				auto reply = try_request(ep, msg);
+				if (reply) {
+					reply->dump();
+					return;
+				}
+				fmt::print("W: no response from {}. Retring...\n", ep);
+			}
+		}
+		fmt::print("W: all servers are disable\n");
+	}
+
+private:
+	std::unique_ptr<zmsg> try_request(std::string_view ep, zmsg& req)
+	{
+		fmt::print("I: trying echo service at {}\n", ep);
+		sock_ = std::make_unique<zmq::socket_t>(ctx_, zmq::socket_type::req);
+		sock_->connect(ep.data());
+		// Configure socket to not wait at close time
+		sock_->set(zmq::sockopt::linger, 0);
+
+		// send request, wait safely for reply
+		zmsg msg(req); // duplicate request
+		msg.send(*sock_);
+
+		std::vector<zmq::pollitem_t> items{
+			{*sock_, 0, ZMQ_POLLIN, 0}
+		};
+
+		zmq::poll(items, REQUEST_TIMEOUT);
+		if (items[0].revents & ZMQ_POLLIN) {
+			return std::make_unique<zmsg>(*sock_);
+		}
+
+		return {};
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2) {
+		fmt::print("Usage: flclient1 [server]\n");
+		return 0;
+	}
+
+	client cli;
+	for (int i = 1; i < argc; ++i) {
+		cli.add(argv[i]);
+	}
+	cli.send_request();
+	return 0;
+}
