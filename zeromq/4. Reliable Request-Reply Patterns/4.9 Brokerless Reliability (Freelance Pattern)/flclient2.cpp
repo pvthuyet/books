69b001001f18e4cbf87e0dce8f97aa0dcd9c0318
pvthuyet <pvthuyet@gmail.com>
2021-07-06 18:29:39 +0700

    Model Two: Brutal Shotgun Massacre

--- zeromq/4. Reliable Request-Reply Patterns/4.9 Brokerless Reliability (Freelance Pattern)/flclient2.cpp
+++ zeromq/4. Reliable Request-Reply Patterns/4.9 Brokerless Reliability (Freelance Pattern)/flclient2.cpp
@@ -6,7 +6,7 @@
 #include <memory>
 #include <thread>
 
-#define REQUEST_TIMEOUT     1000
+#define REQUEST_TIMEOUT     100000
 #define MAX_RETRIES         3       //  Before we abandon
 
 class client
@@ -28,7 +28,9 @@ public:
 
 	void send_request(std::string_view text)
 	{
+		static int seq = 0;
 		zmsg msg(text.data());
+
 		if (servers_.size() == 1) { // try 3 times if 1 server
 			for (int i = 0; i < MAX_RETRIES; ++i) {
 				auto reply = try_request(servers_[0], msg);
@@ -56,13 +58,17 @@ private:
 	std::unique_ptr<zmsg> try_request(std::string_view ep, zmsg& req)
 	{
 		fmt::print("I: trying echo service at {}\n", ep);
-		sock_ = std::make_unique<zmq::socket_t>(ctx_, zmq::socket_type::req);
+		sock_ = std::make_unique<zmq::socket_t>(ctx_, zmq::socket_type::dealer);
 		sock_->connect(ep.data());
 		// Configure socket to not wait at close time
 		sock_->set(zmq::sockopt::linger, 0);
 
 		// send request, wait safely for reply
 		zmsg msg(req); // duplicate request
+		static int seq = 0;
+		msg.wrap(std::to_string(++seq).c_str(), nullptr);
+		msg.wrap("", nullptr);
+
 		msg.send(*sock_);
 
 		std::vector<zmq::pollitem_t> items{
@@ -81,7 +87,7 @@ private:
 int main(int argc, char* argv[])
 {
 	if (argc < 2) {
-		fmt::print("Usage: flclient1 [server]\n");
+		fmt::print("Usage: flclient2 [server]\n");
 		return 0;
 	}
 
---
+++ zeromq/4. Reliable Request-Reply Patterns/4.9 Brokerless Reliability (Freelance Pattern)/flserver2.cpp
@@ -0,0 +1,80 @@
+#include <Windows.h>
+#include <fmt/core.h>
+#include <vld.h>
+#include <zhelpers.hpp>
+#include <zmsg.hpp>
+#include <memory>
+#include <thread>
+#include <gsl/gsl_assert>
+
+class server
+{
+private:
+	zmq::context_t ctx_;
+	std::unique_ptr<zmq::socket_t> socket_;
+	std::unique_ptr<std::jthread> thread_;
+
+public:
+	server() : ctx_(1)
+	{}
+
+	void start(zmq::socket_type type, std::string_view endpoint)
+	{
+		socket_ = std::make_unique<zmq::socket_t>(ctx_, type);
+		socket_->bind(endpoint.data());
+
+		thread_ = std::make_unique<std::jthread>([this](std::stop_token stk) {
+			this->run(stk);
+			});
+		fmt::print("I: echo service is ready at {}\n", endpoint);
+	}
+
+	void stop()
+	{
+		if (thread_ && thread_->joinable()) {
+			thread_->request_stop();
+			thread_->join();
+		}
+	}
+
+private:
+	void run(std::stop_token stk)
+	{
+		fmt::print("server START\n");
+		while (!stk.stop_requested()) {
+			std::vector<zmq::pollitem_t> items = {
+				{*socket_, 0, ZMQ_POLLIN, 0}
+			};
+			zmq::poll(items, 1000);
+			if (items[0].revents & ZMQ_POLLIN) {
+				// request
+				zmsg msg(*socket_);
+				auto sz = msg.parts();
+				Expects(msg.parts() == 2);
+				auto ident = msg.unwrap2();
+				// reply
+				zmsg reply(fmt::format("{} => OK", msg.body()).c_str());
+				reply.wrap(ident.c_str(), nullptr);
+				reply.send(*socket_);
+			}
+		}
+		fmt::print("server END\n");
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2) {
+		fmt::print("Usage: flserver1 [endpoint]\n");
+		return 0;
+	}
+
+	server srv;
+	srv.start(zmq::socket_type::rep, "tcp://*:5555");
+
+	fmt::print("Press ENTER to stop server\n");
+	std::cin.get();
+	srv.stop();
+
+	return 0;
+}
