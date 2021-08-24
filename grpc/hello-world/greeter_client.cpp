50ca091fca4fe794217517fd167f73b03df88ff3
pvthuyet <pvthuyet@gmail.com>
2021-08-25 06:18:49 +0700

    hello world * generate file ok

---
+++ grpc/hello-world/greeter_client.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#include "helloworld.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
+
+int main()
+{
+
+}
