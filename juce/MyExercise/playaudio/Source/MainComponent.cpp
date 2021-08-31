57856a569a701ddab69584093233c151e96cefd8
pvthuyet <pvthuyet@gmail.com>
2021-08-31 11:11:38 +0700

    Tutorial: Build an audio player

---
+++ juce/MyExercise/playaudio/Source/MainComponent.cpp
@@ -0,0 +1,184 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+    : state(Stopped)
+{
+    // Open button
+    addAndMakeVisible(&openButton);
+    openButton.setButtonText("Open...");
+    openButton.onClick = [this] { openButtonClicked(); };
+
+    // Play button
+    addAndMakeVisible(&playButton);
+    playButton.setButtonText("Play");
+    playButton.onClick = [this] { playButtonClicked(); };
+    playButton.setColour(juce::TextButton::buttonColourId, juce::Colours::green);
+    playButton.setEnabled(false);
+    
+    // Stop button
+    addAndMakeVisible(&stopButton);
+    stopButton.setButtonText("Stop");
+    stopButton.onClick = [this] {stopButtonClicked(); };
+    stopButton.setColour(juce::TextButton::buttonColourId, juce::Colours::red);
+    stopButton.setEnabled(false);
+
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (300, 200);
+
+    formatManager.registerBasicFormats();
+    transportSource.addChangeListener(this);
+
+    // Some platforms require permissions to open input channels so request that here
+    if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
+        && ! juce::RuntimePermissions::isGranted (juce::RuntimePermissions::recordAudio))
+    {
+        juce::RuntimePermissions::request (juce::RuntimePermissions::recordAudio,
+                                           [&] (bool granted) { setAudioChannels (granted ? 2 : 0, 2); });
+    }
+    else
+    {
+        // Specify the number of input and output channels that we want to open
+        setAudioChannels (2, 2);
+    }
+}
+
+MainComponent::~MainComponent()
+{
+    // This shuts down the audio device and clears the audio source.
+    shutdownAudio();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    // This function will be called when the audio device is started, or when
+    // its settings (i.e. sample rate, block size, etc) are changed.
+
+    // You can use this function to initialise any resources you might need,
+    // but be careful - it will be called on the audio thread, not the GUI thread.
+
+    // For more details, see the help for AudioProcessor::prepareToPlay()
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    // Your audio-processing code goes here!
+    if (readerSource.get() != nullptr) {
+        transportSource.getNextAudioBlock(bufferToFill);
+        return;
+    }
+
+    // For more details, see the help for AudioProcessor::getNextAudioBlock()
+
+    // Right now we are not producing any data, in which case we need to clear the buffer
+    // (to prevent the output of random noise)
+    bufferToFill.clearActiveBufferRegion();
+}
+
+void MainComponent::releaseResources()
+{
+    // This will be called when the audio device stops, or when it is being
+    // restarted due to a setting change.
+
+    // For more details, see the help for AudioProcessor::releaseResources()
+    transportSource.releaseResources();
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    // You can add your drawing code here!
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+    openButton.setBounds(10, 10, getWidth() - 20, 20);
+    playButton.setBounds(10, 40, getWidth() - 20, 20);
+    stopButton.setBounds(10, 70, getWidth() - 20, 20);
+}
+
+void MainComponent::changeListenerCallback(juce::ChangeBroadcaster* source)
+{
+    if (source == &transportSource) 
+    {
+        if (transportSource.isPlaying())
+            changeState(Playing);
+        else 
+            changeState(Stopped);
+    }
+}
+
+void MainComponent::openButtonClicked()
+{
+    chooser = std::make_unique<juce::FileChooser>("Select a Wave file to play...",
+        juce::File{},
+        "*.wav"
+        );
+    auto chooserFlags = juce::FileBrowserComponent::openMode;
+    chooser->launchAsync(chooserFlags, [this](const juce::FileChooser& fc)
+        {
+            auto file = fc.getResult();
+            if (file != juce::File{})
+            {
+                auto* reader = formatManager.createReaderFor(file);
+                if (reader != nullptr)
+                {
+                    auto newSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+                    transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+                    playButton.setEnabled(true);
+                    readerSource.reset(newSource.release());
+                }
+            }
+        });
+}
+
+void MainComponent::playButtonClicked()
+{
+    changeState(Starting);
+}
+
+void MainComponent::stopButtonClicked()
+{
+    changeState(Stopping);
+}
+
+void MainComponent::changeState(TransportState newState)
+{
+    if (state == newState)
+        return;
+
+    state = newState;
+    switch (state)
+    {
+    case MainComponent::Stopped:
+        stopButton.setEnabled(false);
+        playButton.setEnabled(true);
+        transportSource.setPosition(0.0);
+        break;
+
+    case MainComponent::Starting:
+        playButton.setEnabled(false);
+        transportSource.start();
+        break;
+
+    case MainComponent::Playing:
+        stopButton.setEnabled(true);
+        break;
+
+    case MainComponent::Stopping:
+        transportSource.stop();
+        break;
+
+    default:
+        break;
+    }
+}
